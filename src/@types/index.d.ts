cc4dd1291955e56425cd152f80e43e10101dbf6f
David <contato.davidalmeida@outlook.com>
2024-01-31 14:55:27 -0300

    🏷️ Add or update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,15 +2,27 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.webp";
 
-interface ButtonProps {
+interface ButtonBaseProps {
   children: React.ReactNode;
-  type?: "submit" | "button" | "reset" | undefined;
-  color?: "primary" | "secondary" | "tertiary";
-  size?: "xs" | "sm" | "md" | "lg";
-  classname?: string;
-  arialabel: string;
+  type: "submit" | "button" | "reset" | undefined;
+  rounded?: boolean;
+  ariaLabel: string;
   onClick?: (e: any) => void;
 }
+
+interface ButtonProps extends ButtonBaseProps {
+  color: "primary" | "secondary" | "tertiary";
+  size: "xs" | "sm" | "md" | "lg";
+}
+
+type StyledButton = pick<ButtonProps, "color", "size">;
+
+interface IconButtonProps extends ButtonBaseProps {
+  color: "primary" | "secondary" | "tertiary" | "quartiary";
+  size: "xs" | "sm" | "md";
+}
+
+type StyledIconButton = pick<IconButtonProps, "color", "size">;
 
 interface StyledAnimationProps {
   children: React.ReactNode;
